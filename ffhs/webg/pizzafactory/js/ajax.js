c74ea3f600b8e020139592cddd99f64e40bacc5a
Oliver Bucher <nimboline@gmail.com>
2018-12-30 20:36:52 +0100

    commit

--- ffhs/webg/pizzafactory/js/ajax.js
+++ ffhs/webg/pizzafactory/js/ajax.js
@@ -1,6 +1,13 @@
 // AJAX
 
-var req = new XMLHttpRequest();
-req.open("GET", "https://tonyspizzafactory.herokuapp.com/swagger#/auth/Authorize", false);
-req.send(null);
-console.log(req.responseText);
+var xhr = new XMLHttpRequest();
+var url = "https://tonyspizzafactory.herokuapp.com/api/pizzas";
+xhr.open("GET", url, true);
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    var json = JSON.parse(xhr.responseText);
+    console.log(json);
+  }
+};
+xhr.setRequestHeader("Authorization", "TOKEN");
+xhr.send(null);
